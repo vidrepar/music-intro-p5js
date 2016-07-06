84f256a6a23081bac1b65a235b0c56383cb5e5b3
vidrepar <vid.repar@gmail.com>
2016-07-06 14:07:45 +0200

    progress: added FTT(frequency) and calculated average; resizing based on average FTT; added rectangle, circle and triangle for different average frequency levels

--- js/app.js
+++ js/app.js
@@ -1,27 +1,18 @@
 var song;
 var button;
-var jumpButton;
+var amp;
+var fft;
 
 function setup() {
 
-    createCanvas(200, 200);
+    createCanvas(windowHeight, windowHeight-50);
     song = loadSound('assets/sound/song.mp3', loaded);
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-    jumpButton = createButton('jump');
-    jumpButton.mousePressed(jumpSong);
+    //amp = new p5.Amplitude();
+
+    fft = new p5.FFT();
+    fft.setInput(song);
 
     background(51);
-
-    song.addCue(2, changeBackground, color(0,0,255));
-    song.addCue(4, changeBackground, color(0,255,255));
-    song.addCue(6, changeBackground, color(255,255,255));
-
-}
-
-function changeBackground(col) {
-
-    background(col);
 
 }
 
@@ -30,6 +21,64 @@ function changeBackground(col) {
 * */
 function loaded() {
     console.log('loaded');
+
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
+
+}
+
+function draw() {
+
+    background(51);
+
+    //var vol = amp.getLevel();
+    var spectrum = fft.analyze();
+    var diam;
+    var fttSum;
+    var fttAvg;
+
+    console.log(spectrum);
+    //console.log(vol);
+
+    for (i = 0; i<spectrum.length; i++) {
+
+        diam = map(spectrum[i], 0, 255, 10, 200);
+        //fill(255, 0, 255);
+
+
+
+
+        fttSum = 0;
+        for( var i = 0; i < spectrum.length; i++ ){
+            fttSum += parseInt( spectrum[i], 10 ); //don't forget to add the base
+        }
+
+        fttAvg = Math.floor(fttSum/spectrum.length);
+
+    }
+
+    console.log(fttAvg);
+
+    if (fttAvg > 10 && fttAvg < 20) {
+        fill(255, 255, 255);
+        ellipse(width / 2, height / 2, diam, diam);
+    } else if (fttAvg > 20 && fttAvg < 30) {
+        rectMode(RADIUS);
+        fill(255, 23, 35);
+        rect(width / 2, height / 2, diam, diam);
+    } else if (fttAvg > 30) {
+        rectMode(RADIUS);
+        fill(200, 135, 255);
+        triangle(width/2, 0+diam*2, 0+diam*2, height-diam*2, width-diam*2, height-diam*2);
+    }
+
+
+    /*beginShape();
+    for (i = 0; i<spectrum.length; i++) {
+        vertex(i, map(spectrum[i], 0, 255, height, 0) );
+    }
+    endShape();*/
+
 }
 
 function togglePlaying() {
@@ -43,8 +92,3 @@ function togglePlaying() {
         button.html('play');
     }
 }
-
-function jumpSong() {
-    
-}
-
